40261e71e36ded475224c2f86d41968bce586b0a
Saurabh Pandey <saurabhpandey59373@gmail.com>
2023-04-09 18:04:04 +0530

    Added search button, clicking searchButton takes you to search Page

--- app/src/main/java/com/example/floodprediction/MainActivity.kt
+++ app/src/main/java/com/example/floodprediction/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.floodprediction
-package com.chaquo.myapplication
 
 import android.content.Context
+import android.content.Intent
 import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.view.inputmethod.InputMethodManager
@@ -10,24 +10,17 @@ import android.widget.EditText
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.chaquo.python.PyException
-import com.chaquo.python.Python
-import com.chaquo.python.android.AndroidPlatform
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if (! Python.isStarted()) {
-            Python.start(AndroidPlatform(this))
+        var searchButton = findViewById<Button>(R.id.searchButton)
+        searchButton.setOnClickListener {
+            val intent1 = Intent(this, searchPage::class.java)
+            startActivity(intent1)
         }
-
-        val py = Python.getInstance()
-        val name = Python.getModule("hello")
-
-        println(name)
     }
 }
---
+++ app/src/main/java/com/example/floodprediction/searchPage.kt
@@ -0,0 +1,13 @@
+package com.example.floodprediction
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class searchPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_page)
+
+
+    }
+}
