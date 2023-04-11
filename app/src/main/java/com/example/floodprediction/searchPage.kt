bfe77d67fa55a740650e0fddac6a149044f93632
Saurabh Pandey <saurabhpandey59373@gmail.com>
2023-04-12 00:48:17 +0530

    Added search button, clicking searchButton takes you to search Page, Allowed API calls to weatherapi.com

--- app/src/main/java/com/example/floodprediction/searchPage.kt
+++ app/src/main/java/com/example/floodprediction/searchPage.kt
@@ -2,12 +2,13 @@ package com.example.floodprediction
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.chromium.net.CronetEngine
 
 class searchPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search_page)
-
-
     }
+
+
 }
